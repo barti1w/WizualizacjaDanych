5794c446a0965df8d0eebacbc554ab36ba11c87d
Bartek <bartiwojciechowski@wp.pl>
2020-02-27 14:31:07 +0100

    komit

---
+++ Zadania.py
@@ -0,0 +1,12 @@
+a=8
+b=4
+c=3
+dzielenie = a/b
+print (dzielenie)
+dzielenie = a/c
+print(dzielenie)
+dodawanie_całkowite = a//v
+print (dzielenie_całkowite)
+reszta = a%c
+print(reszta)
+
---
+++ start.py
@@ -0,0 +1,59 @@
+print("Hello world")
+tekst = "Hello world"
+tekst = 'Hello world'
+tekst = """Hello
+awidjwi
+ world"""
+print(tekst)
+print(type(tekst))
+print(type(5))
+print(type(5.5))
+print(type(True))
+print(type(None))
+print(5**5) #potęgowanie
+print(5//5) #dzielenie bez reszty
+liczba = int("100")
+print ("Ala " *10)
+
+#listy
+lista=[]
+print(type(lista))
+lista2 = [1,2,3]
+print(lista2[0])
+imie = "Magdalena"
+print(imie[0])
+# lista[0]=5
+# imie[0]="K" błąd
+imie = "Kagdalena"
+imie = imie.swapcase()
+print(imie)
+print(imie.swapcase())
+"Ala".swapcase()
+lista3 = [1,"Ala",4.5,None,True,[1,2]]
+lista3[5][1]
+
+macierz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print(macierz[1][1]) #5
+nowa = lista2 + macierz
+#słownik[klucz] = nazwa
+slownik = {}
+slownik['imie']='Adam'
+slownik[0] = 'Adam'
+print(slownik)
+slownik2 = {'imie': 'Adam', 0: 'Adam'}
+print(slownik2.keys())
+print(slownik2.values())
+
+def pow():
+pass
+
+from math import *
+import math as 
+from math import pow
+m.pow()
+for element in kolekcja
+pow()
